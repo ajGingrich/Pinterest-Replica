e8330c499a55dd34381da71c4da66b786880ed34
Andrew Gingrich <ajgingrich@yahoo.com>
2017-07-24 20:58:25 -0400

    add like capabilites

--- app/controllers/pictureHandler.server.js
+++ app/controllers/pictureHandler.server.js
@@ -27,12 +27,12 @@ function pictureHandler () {
             description: imgDescription,
             userId: userId,
             username: username,
-            likes: ['test', 'todo']
+            likes: []
         });
 
         newImage.save(function(err) {
             if (err) throw err;
-            return next();
+            res.redirect('/myPics');
         });
 
     };
@@ -44,7 +44,6 @@ function pictureHandler () {
         //find all the pics
         Image.find({}, function(err, doc) {
             if (err) throw err;
-           //console.log(doc);
            res.locals.allPics = doc;
            return next();
         });
@@ -53,16 +52,87 @@ function pictureHandler () {
 
     this.getUserPics = function(req, res, next) {
         res.locals.userPics = [];
-        console.log(req.user);
 
         //find all user's pics
         Image.find({userId: req.user._id}, function(err, doc) {
             if (err) throw err;
-            //console.log(doc);
             res.locals.userPics = doc;
             return next();
         });
     };
+
+    this.removePic = function(req, res, next) {
+
+        var userId = req.user._id;
+        var picId = req.params.picId;
+
+        ///find the pic
+        Image.findById(picId, function (err, doc) {
+            if (err) throw err;
+
+            ///only allow user to delete their own pic
+            if (doc.userId == userId) {
+
+                //remove it
+                Image.findByIdAndRemove(picId, function(err) {
+                    if (err) throw err;
+                    res.redirect('/');
+                });
+            }
+            else {
+                res.locals.allPics = [];
+                Image.find({}, function(err, doc) {
+                    if (err) throw err;
+                    res.locals.allPics = doc;
+                    res.render('index', {message: 'You can only delete your own pictures', user: req.user});
+                });
+            }
+        });
+    };
+
+    this.likePic = function(req, res, next) {
+
+        var userId = req.user._id;
+        var picId = req.params.picId;
+
+        //add user to picture's like array
+        Image.findByIdAndUpdate(picId, { $push: { likes: userId }}, {new: true}, function(err, doc){
+            if (err) throw err;
+
+            //redirect back to where it came from
+            if (req.headers.referer.indexOf('myPics') != -1) {
+                res.locals.userPics = doc;
+                res.redirect('/myPics');
+            }
+            else {
+                res.locals.allPics = doc;
+                res.redirect('/');
+            }
+        })
+
+    };
+
+    this.dislikePic = function(req, res, next) {
+
+        var userId = req.user._id;
+        var picId = req.params.picId;
+        
+        //add user to picture's like array
+        Image.findByIdAndUpdate(picId, { $pull: { likes: userId }}, {new: true}, function(err, doc){
+            if (err) throw err;
+
+            //redirect back to where it came from
+            if (req.headers.referer.indexOf('myPics') != -1) {
+                res.locals.userPics = doc;
+                res.redirect('/myPics');
+            }
+            else {
+                res.locals.allPics = doc;
+                res.redirect('/');
+            }
+        })
+    };
+
 
 
 }
--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,39 @@ var passport = require('passport');
 var router = express.Router();
 var PictureHandler = require('../app/controllers/pictureHandler.server');
 var pictureHandler = new PictureHandler();
+var Image = require('../app/models/images.js');
 
 //home page
 router.get('/', pictureHandler.getAllPics, function(req, res) {
-    res.render('index');
+    res.render('index', {user: req.user});
 });
 
 //myPics
 router.get('/myPics', isLoggedIn, pictureHandler.getUserPics, function(req, res) {
-    res.render('userPics');
+    res.render('userPics', {user: req.user});
 });
 
 //allUsers
 router.get('/allUsers', function(req, res) {
-    res.render('allUsers');
+    res.render('allUsers', {user: req.user});
 });
 
 //add a picture
-router.post('/addPic', isLoggedIn, pictureHandler.addPic, pictureHandler.getUserPics, function(req, res) {
-    res.render('userPics');
-});
+router.post('/addPic', isLoggedIn, pictureHandler.addPic);
 
-///test and delete late
-/*router.post('/addPic', pictureHandler.addPic, pictureHandler.getAllPics, function(req, res) {
- res.render('index');
-});*/
+//remove a picture
+router.get('/removePic/:picId', isLoggedIn, pictureHandler.removePic);
+
+//like a picture
+router.get('/likePic/:picId', isLoggedIn, pictureHandler.likePic);
+
+//like a picture
+router.get('/dislikePic/:picId', isLoggedIn, pictureHandler.dislikePic);
 
 //logout
 router.get('/logout', function(req, res) {
     req.logout();
-    res.redirect('/myPics');
+    res.redirect('/myPics', {user: req.user});
 });
 
 //authentications for facebook, twitter and google.
@@ -65,5 +68,13 @@ module.exports = router;
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
-    res.redirect('/');
+    //console.log(req.path)
+
+    res.locals.allPics = [];
+    Image.find({}, function(err, doc) {
+        if (err) throw err;
+        res.locals.allPics = doc;
+        res.render('index', {message: 'You need to be logged in', user: req.user});
+    });
+
 }
